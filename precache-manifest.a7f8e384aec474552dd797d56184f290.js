70780ab493587a5dcda8a39139058411d3630db6
Nivedita Gautam <niveditagautam000@gmail.com>
2019-09-08 22:58:39 -0500

    Updates

---
+++ precache-manifest.a7f8e384aec474552dd797d56184f290.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5dd3124a68c571425d872e1353eaf2a",
+    "url": "/npat/index.html"
+  },
+  {
+    "revision": "244e338bb261c597e876",
+    "url": "/npat/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "16a03fd46af7c7051bcd",
+    "url": "/npat/static/js/2.556f3074.chunk.js"
+  },
+  {
+    "revision": "244e338bb261c597e876",
+    "url": "/npat/static/js/main.b92bbdfb.chunk.js"
+  },
+  {
+    "revision": "aff67060ffa8a9c75089",
+    "url": "/npat/static/js/runtime~main.1b71e7ee.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/npat/static/media/logo.5d5d9eef.svg"
+  }
+]);
